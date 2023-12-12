543bd6c4fc651cdd9dec6ace115d92c9b2e5dbe1
max-dieckmann <max.dieckmann@novanta.com>
2023-12-12 12:28:55 +0100

    DR0053A-127 Type ignore pre-commit fix

--- run-mypy.sh
+++ run-mypy.sh
@@ -3,5 +3,7 @@
 set -o errexit
 # Change directory to the project root directory.
 cd "$(dirname "$0")"
+# Ignore auto-generated file
+sed -i "1s/.*/# type: ignore/" resources_rc.py
 # Run mypy using the config file
 mypy . --config-file mypy.ini
